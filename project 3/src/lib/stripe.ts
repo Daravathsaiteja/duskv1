76587f5cdc533064d133abb279b99cafe14d3997
Daravathsaiteja <90112511+Daravathsaiteja@users.noreply.github.com>
2025-04-09 07:26:31 +0100

    Delete project 3 directory

--- project 3/src/lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePublicKey = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
-
-if (!stripePublicKey) {
-  throw new Error('Missing Stripe public key');
-}
-
-export const stripe = loadStripe(stripePublicKey);
