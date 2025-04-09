14befb1fc808006eca7fedc512e25c7923afabb9
Daravathsaiteja <90112511+Daravathsaiteja@users.noreply.github.com>
2025-04-09 08:10:28 +0100

    Delete project directory

--- project/src/components/PromoBar.tsx
+++
@@ -1,7 +0,0 @@
-export function PromoBar() {
-  return (
-    <div className="bg-black py-2 text-center text-sm text-white">
-      Free Shipping on Orders Over $50 | Easy Returns
-    </div>
-  );
-}
