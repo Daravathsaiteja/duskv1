65a307ca81c7db933dbce0cd6e284503c4ad9d09
Daravathsaiteja <90112511+Daravathsaiteja@users.noreply.github.com>
2025-04-09 06:12:19 +0100

    Update Logo.tsx

--- project-V0.2/src/components/Logo.tsx
+++ project-V0.2/src/components/Logo.tsx
@@ -1,4 +1,4 @@
-import { Cloud, CloudLightning } from 'lucide-react';
+import { CloudLightning } from 'lucide-react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
